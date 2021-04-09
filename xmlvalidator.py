592ce40b8fb0d62416b1cee56e5152e8b1a7f725
Per von Rosen <per.von.rosen@gmail.com>
2021-04-09 23:54:03 +0200

    a

--- xmlvalidator.py
+++ xmlvalidator.py
@@ -1,15 +1,19 @@
 import streamlit as sf
 from lxml.etree import parse, XMLSchema
 
-sf.write("XML validator")
-XSDf = sf.file_uploader("XSD file")
-XMLf = sf.file_uploader("XML file")
 
-if XMLf!=None and XSDf!=None:
-   xml_file = parse(XMLf)
-   xml_validator = XMLSchema(parse(XSDf))
+def main():
+   sf.write("XML validator")
+   XSDf = sf.file_uploader("XSD file")
+   XMLf = sf.file_uploader("XML file")
 
-   is_valid = xml_validator.validate(xml_file)
+   if XMLf!=None and XSDf!=None:
+      xml_file = parse(XMLf)
+      xml_validator = XMLSchema(parse(XSDf))
 
-   sf.text("Is valid: {}".format(is_valid))
+      is_valid = xml_validator.validate(xml_file)
 
+      sf.text("Is valid: {}".format(is_valid))
+
+if __name__ == "__main__":
+    main()
